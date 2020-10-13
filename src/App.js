88ff22ab717357626b4a7c2588fa40d9f0408713
David Dales <david.c.dales@gmail.com>
2020-10-13 09:46:31 -0400

    enabled ESlint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+const test_API =
+  "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=TSLA&apikey=NOA4WSXR5B8R43F5";
 
-const test_API = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=TSLA&apikey=NOA4WSXR5B8R43F5";
-
-fetch(test_API).then(response => {
-  return response.json();
-}).then(data => {
-  console.log(data);
-});
+fetch(test_API)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
 
 function App() {
   return (
